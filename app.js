4d465f2950e1e9faf136d8e22839018ea6d28b3c
Melissa Seago <melissa.seago@gmail.com>
2016-04-12 18:13:48 -0500

    first commit for express js

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function() {
+  console.log('app is running on port 3000!');
+});
+
+console.log(__dirname);
